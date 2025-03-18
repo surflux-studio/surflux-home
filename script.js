47af9f30ab8008d9512d9ada21383017091bf677
carl <carl@surflux.studio>
2025-03-18 11:29:57 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  // 버튼 클릭 이벤트
+  // const aboutBtn = document.querySelector('.about-btn')
+  // const contactBtn = document.querySelector('.contact-btn')
+  // aboutBtn.addEventListener('click', function () {
+  //   console.log('Try Free clicked')
+  // })
+  // contactBtn.addEventListener('click', function () {
+  //   console.log('Book Demo clicked')
+  // })
+})
